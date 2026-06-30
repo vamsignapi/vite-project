5989eea14314d5dfb05d4d3fa030de75c39166d2
Vamsi Gnapi <vamsignapi@gmail.com>
2026-06-30 16:29:33 +0530

    Failure test

--- tests/counter.test.ts
+++ tests/counter.test.ts
@@ -18,7 +18,7 @@ describe("setupCounter", () => {
     setupCounter(button);
 
     button.click();
-    expect(button.innerHTML).toBe("Count is 1");
+    expect(button.innerHTML).toBe("Count is 2");
 
     button.click();
     expect(button.innerHTML).toBe("Count is 2");
